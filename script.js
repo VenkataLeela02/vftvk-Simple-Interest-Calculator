5fc6b081cbc216fc3e5adc72490ab20ef8f8f6e9
VenkataLeela02 <33417967+VenkataLeela02@users.noreply.github.com>
2021-05-17 15:34:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+
+var interest_rate;
+	function interestValue(x){
+	document.getElementById("interest").innerHTML = x+" %";
+	}
+	
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	var principal = document.getElementById("principal").value;
+	var interest = document.getElementById("rate").value;
+	
+	if(principal === "" || principal == '0'){
+	
+		document.getElementById("result").style.display = "none";
+		document.getElementById("error").style.display = "block";
+		document.getElementById("error").innerHTML = "Please enter the principal amount";
+		
+	}
+	else{
+		
+		var e = document.getElementById("years");
+		var years = e.options[e.selectedIndex].value;
+		
+		document.getElementById("result").style.display = "block";
+		document.getElementById("error").style.display = "none";
+		document.getElementById("res_principal").innerHTML = principal;
+		document.getElementById("res_rate").innerHTML = interest;
+		
+			
+		var date = new Date();
+		var res_years = (date.getFullYear()) + parseInt(years,10);
+		console.log(res_years);
+			
+		
+		document.getElementById("res_years").innerHTML = res_years;
+		var maturity_interest =  Math.floor(principal * interest * years/100);
+		document.getElementById("cal_interest").innerHTML = maturity_interest;
+	}
 }
         
